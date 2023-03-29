4d22b0e50b633a552a284f388875ff5f611bd692
nadaronda <rosarioplazasaray@hotmail.com>
2023-03-29 16:41:43 +0200

    intentar metodo delete

--- frontend/components/form/Card.tsx
+++ frontend/components/form/Card.tsx
@@ -1,5 +1,7 @@
+import { interfaceProduct } from "../../types/interfaceProduct";
 
-export const Card: React.FC<{ price: number, nameProduct: string, description: string; }> = ({ price, description, nameProduct }) => {
+
+export const Card: React.FC<interfaceProduct> = ({ price, description, nameProduct, _id, active }) => {
 
     return (<>
         <div className="w-[18rem] h-[18rem] px-3 py-3 bg-neutral-50 rounded-lg border-2 border-solid border-yellow-300" >
@@ -7,11 +9,17 @@ export const Card: React.FC<{ price: number, nameProduct: string, description: s
                 <h5 className="text-center ">{nameProduct}</h5>
                 <br />
                 <p>Descripción: {description}</p>
-                <p className="">Precio: {price} €</p>
+                <p>Precio: {price} €</p>
+
             </div>
             <div className="flex gap-[2px]">
-                <button className="bg-slate-500 rounded-l-lg text-white px-3 hover:bg-slate-400 hover:text-black"> BAJA</button>
-                <button className="bg-slate-500 rounded-r-lg  text-white px-3 hover:bg-slate-400 hover:text-black">BORRAR</button>
+
+                <button className="bg-slate-500 rounded-l-lg text-white px-3 hover:bg-slate-400 hover:text-black">
+                    BAJA
+                </button>
+                <button className="bg-slate-500 rounded-r-lg  text-white px-3 hover:bg-slate-400 hover:text-black">
+                    BORRAR
+                </button>
             </div>
 
         </div>
---
+++ frontend/types/interfaceProduct.ts
@@ -0,0 +1,10 @@
+export interface interfaceProduct {
+    //category?: String,
+    _id?: String,
+    nameProduct: String,
+    description: String,
+    price: any,
+    active?: Boolean,
+
+    // add seed categorys predefinidas
+}
